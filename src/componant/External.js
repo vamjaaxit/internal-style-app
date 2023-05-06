b6798c7aff8ad80b9d21b0a71b7568712fff85c2
vamjaaxit <vamjaaxit111@gmail.com>
2023-05-06 13:40:43 +0530

    uplod

---
+++ src/componant/External.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const book ="assest/images/gif.gif";
+function Axit()
+{
+return(
+
+
+    <>
+    <section id="main">
+        <div className="content1">
+
+<h1>book your Appointment with us</h1>
+<hr align="left"/>
+<p>hospital address:kalaward road nea st mary's high school,rajkot.gujrat 360007</p>
+<p><b>E-mail us:</b><a href="">axitpatel2882@gmail.com</a></p>
+<p><b>contact us:</b>(+91)9099225161</p>
+<img src={book} alt="image01" title="axit"></img>
+        </div>
+
+        <div className="content2">
+
+            
+        </div>
+
+
+
+    </section>
+    
+    
+    </>
+)
+};
+export default Axit
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import Reportwebvitals from './reportWebVitals'
+import './style.css';
+import Axit from "./componant/External";
+
+
+const root=ReactDOM.createRoot(document.getElementById("axit"));
+root.render(
+<Axit/>
+);
+
+
+
+
+
+Reportwebvitals();
